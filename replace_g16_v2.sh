7c9b624788493f9a904c1f588e464dd21d9f2c6f
gaalvarezu <147661697+gaalvarezu@users.noreply.github.com>
2023-10-27 13:51:21 +0200

    Update replace_g16_v2.sh
    replace with hability to edit the instructions line in a gaussian's *.gjf input file

--- replace_g16_v2.sh
+++ replace_g16_v2.sh
@@ -1,1 +1,11 @@
-sed -i 's/utpsstpss/utpsstpss\/def2svp/' ./cdc_axi_exo_E03_I2.gjf
+ #!/bin/bash
+job1=$1
+job2=$2
+functional=$3
+base=$4
+ScriptDir="$(dirname "$0")"
+for file in *.gjf; do
+       sed -i "/^#/ s/#.*/# $job1 $job2 $functional\/$base empiricaldispersion\=gd3bj scrf\=(\solvent\=acetonitrile,pcm) guess\=mix/" $file
+       sed -i 's/\r//' $file
+done
+
